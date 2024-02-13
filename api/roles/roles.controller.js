d9e4291b4707ed7d11af834cb3cb29f867ae4f02
Jonathan Rivera <jacuchimba@stefanini.com>
2024-02-13 09:12:30 -0500

    Ajuste data y controller roles

--- api/roles/roles.controller.js
+++ api/roles/roles.controller.js
@@ -33,28 +33,75 @@ class rolesController {
   }
 
   static getRolParameters(req, res) {
-    const query = req.query;
+    const queryParamsRoles = {
+      idRole: req.query.idRole,
+      idState: req.query.idState,
+      startDate: req.query.startDate,
+      endDate: req.query.endDate,
+    };
 
-    const filterData = (data, query) =>
-      data.filter((rec) =>
-        Object.entries(query).every(([k, v]) => rec[k].toString().includes(v))
-      );
+    for (const i in queryParamsRoles) {
+      if (queryParamsRoles[i] === undefined) {
+        delete queryParamsRoles[i];
+      }
+    }
 
-    const resultData = filterData(data.roles, query);
+    if (Object.keys(queryParamsRoles).length === 0) {
+      const paginationSize = req.query.paginationSize || 10;
+      const paginationKey = req.query.paginationKey || 0;
+      const start = paginationSize * paginationKey;
+      const end = Number(start) + Number(paginationSize);
+      const data = dataRoles.roles.slice(start, end);
+      const totalData = dataRoles.roles.length;
+      const pagesSize = totalData / paginationSize;
 
-    // Response 200
-    res.status(200).json({
-      status: 200,
-      message: "Successful",
-      data: resultData,
-      totalElements: resultData.length,
-    });
-  
-    // // Response 500
-    // res.status(500).json({
-    //   status: 500,
-    //   message: "Error 500",
-    // });
+      // Response 200
+      res.status(200).json({
+        status: 200,
+        message: "Successful",
+        data: data,
+        pagination: {
+          totalPages: pagesSize,
+          totalElements: totalData,
+        },
+      });
+
+      // // Response 500
+      // res.status(500).json({
+      //   status: 500,
+      //   message: "Error 500",
+      // });
+    } else {
+      const filterData = (data, query) =>
+        data.filter((rec) =>
+          Object.entries(query).every(([k, v]) => rec[k].toString().includes(v))
+        );
+      const resultData = filterData(dataRoles.roles, queryParamsRoles);
+      const paginationSize = req.body.paginationSize || 10;
+      const paginationKey = req.body.paginationKey || 0;
+      const start = paginationSize * paginationKey;
+      const end = Number(start) + Number(paginationSize);
+      const data = resultData.slice(start, end);
+      const totalData = resultData.length;
+      const pagesSize = totalData / paginationSize;
+
+      // Response 200
+      res.status(200).json({
+        status: 200,
+        message: "Successful",
+        data: data,
+        pagination: {
+          totalPages: pagesSize,
+          totalElements: totalData,
+        },
+      });
+
+      // // Response 500
+      // res.status(500).json({
+      //   status: 500,
+      //   message: "Error 500",
+      // });
+    }
   }
 }
 
