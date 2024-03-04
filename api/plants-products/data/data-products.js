cc84f6fd866cfb0b026f8aa1f86969926ba7ffb8
Jonathan Rivera <jacuchimba@stefanini.com>
2024-03-04 16:23:12 -0500

    Ajuste servicios plantas y productos id tipo uuid

--- api/plants-products/data/data-products.js
+++ api/plants-products/data/data-products.js
@@ -1,47 +1,47 @@
 const response = {
   products: [
     {
-      idProduct: 2101,
+      idProduct: 'b6b8dbb0-218b-4800-aff5-476b46ee1535',
       nameProduct: 'Producto 1',
     },
     {
-      idProduct: 2102,
+      idProduct: '1f2afe1f-1010-4e7a-9ea7-8d552b465218',
       nameProduct: 'Producto 2',
     },
     {
-      idProduct: 2103,
+      idProduct: 'e24dace5-a39b-4d97-b05c-cfed8651d292',
       nameProduct: 'Producto 3',
     },
     {
-      idProduct: 2104,
+      idProduct: '252b8325-96ed-4f70-a9b7-1178ed0cc03b',
       nameProduct: 'Producto 4',
     },
     {
-      idProduct: 2105,
+      idProduct: '252b8325-96ed-4f70-a9b7-1178ed0cc03b',
       nameProduct: 'Producto 5',
     },
     {
-      idProduct: 2106,
+      idProduct: '821ad86a-e9f3-4443-80cd-b7e27f21afc4',
       nameProduct: 'Producto 6',
     },
     {
-      idProduct: 2107,
+      idProduct: '63e80a6a-2ede-4eba-b03e-cbee71b3fd2e',
       nameProduct: 'Producto 7',
     },
     {
-      idProduct: 2108,
+      idProduct: '847ba9b1-16d3-42a5-9778-ac393aee6b03',
       nameProduct: 'Producto 8',
     },
     {
-      idProduct: 2109,
+      idProduct: '7b253db5-b537-40f6-9ba5-8b07cfece099',
       nameProduct: 'Producto 9',
     },
     {
-      idProduct: 2110,
+      idProduct: '15cb504d-9e5a-4be0-b347-469cc6a1e2fd',
       nameProduct: 'Producto 10',
     },
     {
-      idProduct: 2111,
+      idProduct: '979964ff-c902-4c01-9350-2319fd281c75',
       nameProduct: 'Producto 11',
     },
   ],
--- api/plants-products/data/data-type-products.js
+++ api/plants-products/data/data-type-products.js
@@ -1,47 +1,47 @@
 const response = {
   typeProducts: [
     {
-      idTypeProduct: 3101,
+      idTypeProduct: 'ce89e3bf-b4d1-4e9a-b3a8-3c4b3fa8d781',
       nameTypeProduct: 'Tipo de producto 1',
     },
     {
-      idTypeProduct: 3102,
+      idTypeProduct: '637982d4-efb4-46b7-a8cf-4e0aaac88171',
       nameTypeProduct: 'Tipo de producto 2',
     },
     {
-      idTypeProduct: 3103,
+      idTypeProduct: '670fc3bc-3d37-4429-a04d-82444dc0fde8',
       nameTypeProduct: 'Tipo de producto 3',
     },
     {
-      idTypeProduct: 3104,
+      idTypeProduct: '2b7d9caf-8c71-4bb6-a891-9245d6f5bba7',
       nameTypeProduct: 'Tipo de producto 4',
     },
     {
-      idTypeProduct: 3105,
+      idTypeProduct: 'aee4aa0a-4b2a-4a58-8210-a4891b11fbe8',
       nameTypeProduct: 'Tipo de producto 5',
     },
     {
-      idTypeProduct: 3106,
+      idTypeProduct: '2448497c-ff12-48a9-a47f-2247cfa8df36',
       nameTypeProduct: 'Tipo de producto 6',
     },
     {
-      idTypeProduct: 3107,
+      idTypeProduct: 'b08ff307-0bf7-4c58-a89c-1b17be3e590b',
       nameTypeProduct: 'Tipo de producto 7',
     },
     {
-      idTypeProduct: 3108,
+      idTypeProduct: '5ec44fa9-12a8-4d45-b8d9-9c2e880adaa6',
       nameTypeProduct: 'Tipo de producto 8',
     },
     {
-      idTypeProduct: 3109,
+      idTypeProduct: '387b8222-dc13-4dc1-b8cc-119277de5554',
       nameTypeProduct: 'Tipo de producto 9',
     },
     {
-      idTypeProduct: 3110,
+      idTypeProduct: 'd4c5bf25-23ec-4454-bb0f-b4be234462a9',
       nameTypeProduct: 'Tipo de producto 10',
     },
     {
-      idTypeProduct: 3111,
+      idTypeProduct: '0552a30e-8e98-439c-8261-e7494a05cdcf',
       nameTypeProduct: 'Tipo de producto 11',
     },
   ],
